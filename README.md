199f4ff75de2a5e31cdeaff3bed35096ce03c433
0xNekr <0xNekr.dev@gmail.com>
2022-11-22 16:20:40 +0100

    Deployment of the first version for bug hunting

---
+++ README.md
@@ -0,0 +1,198 @@
+
+# Smart Contract Marmottoshis
+
+Document visant à décrire le smart contract du projet Marmottoshis. Ce répertoire contient le code du contrat "MarmottoshisIsERC1155.sol". 
+
+Vous pouvez retrouver le projet sur Twitter : https://twitter.com/Marmottoshis.
+Si vous avez des questions, vous pouvez retrouver l'équipe et la communauté sur le Discord suivant : https://discord.gg/thecryptomasks dans le channel #Marmottoshis
+
+Tous les liens utiles sont ici : https://linktr.ee/Marmottoshis
+
+## Disclaimer
+Les prix en Ethers sur le contrat ne **SONT PAS** les prix qui seront mis en place au déploiement, une communication arrivera à ce sujet plus tard.
+
+Le contrat publié ici est **proche de sa version finale**. Cependant, la publication de ce code vise à démontrer en toute transparence ce qui sera mis en place, mais aussi à **détecter d'éventuelles anomalies** pouvant être présentes. Si vous trouvez un bug/une anomalie, n'hésitez pas à contacter le projet (ou @0xNekr/@DocMarmott sur Twitter).
+
+Si vous êtes le premier à reporter un bug et qu'il est utile pour la suite du projet, vous serez bien sûr **récompensé**. 
+ 
+
+Le contrat dans sa version définitive sera **disponible ici avant le déploiement** et **vérifié sur Etherscan** une fois déployé afin que vous puissiez vous assurer que rien n'a été modifié depuis la phase de "bug hunt" (ou que les corrections ont bien été faites).
+
+
+## Features
+
+**Pour l'utilisateur, ce contrat permet de :**
+
+- Réserver son NFT avant le mint. 
+- Mint son NFT sous forme de plusieurs phases (free mint - réservation mint - mint première whitelist - mint seconde whitelist - mint publique).
+- Consulter combien de Satoshis sont présent sur le contrat.
+- Consulter combien de Satoshis sont adossés à un ID de NFT spécifique.
+- Brûler (burn) un NFT pour déclencher une demande de récupération des Satoshis. 
+
+**Pour la "marmotte", ce contrat permet de :**
+
+- Changer l'adresse de la marmotte actuelle.
+- Ajouter des Satoshis sur le contrat.
+- Retirer des Satoshis sur le contrat. (utile seulement en cas d'erreur, par exemple, un 0 de trop).
+
+**Pour le propriétaire, ce contrat permet de :**
+
+- Modifier l'étape de vente en cours. 
+- Figer les métadonnées.
+- Révéler les NFTs.
+- Modifier l'URI des NFTs (pour révéler les métadonnées).
+- Ajouter des artistes au contrat (pour les métadonnées on-chain).
+- Modifier les routes de Merkle des whitelists.
+- Modifier les différents prix.
+- Récupérer les ETH du contrat.
+- Changer de propriétaire.
+## Fonctions importantes pour l'utilisateur
+
+### Ajouter/Retirer du BTC
+
+Lors d'un ajout ou un retrait de Satoshis sur le contrat, voici ce qu'il se passe : 
+
+- Vérification que la personne qui souhaite ajouter/retirer des Satoshis est bien l'adresse "Marmott".
+- Ajout/retrait du nombre de Satoshis à la balance du contrat.
+- Récupérer le nombre d'ID contenant encore des NFTs afin de partager les Satoshis entre chaque. 
+- Division du nombre de Satoshis par le nombre d'ID avec encore au moins 1 NFT. 
+- Ajouter/retrait du résultat de cette division à la balance de chaque ID encore présent.
+
+```solidity
+function addBTC(uint satoshis) external {
+    require(msg.sender == marmott, "Only Marmott can add BTC");
+    balanceOfSatoshis = balanceOfSatoshis + satoshis;
+    uint divedBy = getNumberOfIdLeft();
+    require(divedBy > 0, "No NFT left");
+    uint satoshisPerId = satoshis / divedBy;
+    for (uint i = 1; i <= maxToken; i++) {
+        if (supplyByID[i] > 0) {
+            balanceOfSatoshiByID[i] = balanceOfSatoshiByID[i] + satoshisPerId;
+        }
+    }
+}
+```
+
+```solidity
+function subBTC(uint satoshis) external {
+    require(msg.sender == marmott, "Only Marmott can sub BTC");
+    balanceOfSatoshis = balanceOfSatoshis - satoshis;
+    uint divedBy = getNumberOfIdLeft();
+    require(divedBy > 0, "No NFT left");
+    uint satoshisPerId = satoshis / divedBy;
+    for (uint i = 1; i <= maxToken; i++) {
+        if (supplyByID[i] > 0) {
+            balanceOfSatoshiByID[i] = balanceOfSatoshiByID[i] - satoshisPerId;
+        }
+    }
+}
+````
+
+### Brûler un BTC pour récupérer les Satoshis
+
+Lorsque vous souhaitez récupérer vos Satoshis, vous allez interagir avec la fonction "burnAndRedeem" qui va brûler votre NFT afin d'émettre un événement (une demande de récupération) qui sera ensuite traité par la Marmotte. Tout ceci pourra se passer depuis le contrat, ou via la dApp. 
+
+La fonction va simplement vérifier que votre NFT existe et que vous possédez bien un exemplaire.
+Si c'est le cas, elle va brûler ce NFT, mettre à jour les balances de Satoshis (la balance totale et celle de votre ID de NFT) et les quantités de NFTs.
+Pour finir, elle déclenche un événement avec votre adresse de transaction, l'ID que vous avez brûlé, le nombre (1), votre adresse Bitcoin renseignée, et le nombre de Satoshis que vous avez pu récupérer.
+
+```solidity
+function burnAndRedeem(uint _idToRedeem, string memory _btcAddress) public nonReentrant {
+    require(_idToRedeem >= 1, "Nonexistent id");
+    require(_idToRedeem <= maxToken, "Nonexistent id");
+    require(balanceOf(msg.sender, _idToRedeem) >= 1, "Not enough Marmott to burn");
+    _burn(msg.sender, _idToRedeem, 1);
+    uint satoshisToRedeem = redeemableById(_idToRedeem);
+    balanceOfSatoshis = balanceOfSatoshis - satoshisToRedeem;
+    balanceOfSatoshiByID[_idToRedeem] = balanceOfSatoshiByID[_idToRedeem] - satoshisToRedeem;
+    supplyByID[_idToRedeem] = supplyByID[_idToRedeem] - 1;
+    emit newRedeemRequest(msg.sender, _idToRedeem, 1, _btcAddress, satoshisToRedeem);
+}
+```
+
+### Réserver un mint
+
+La réservation pour le mint vous permet de déposer un montant d'Ethers (la valeur de la réservation n'est pas encore définie) contre la certitude de pouvoir mint un NFT. 
+Il faut absolument être dans la phase ouverte de réservation, envoyer assez d'Ethers, ne pas avoir déjà whitelist votre adresse et que le nombre de réservations total soit inférieur ou égal à 400.
+Une fois ces conditions passées, votre adresse sera whitelist grâce à un tableau (adresse vers booléen). 
+
+```solidity
+function reservationForWhitelist() external payable nonReentrant {
+    require(currentStep == Step.WLReservation, "Reservation for whitelist is not open");
+    require(msg.value >= reservationPrice, "Not enought ether");
+    require(reservationList[msg.sender] == false, "You are already in the pre-whitelist");
+    require(currentReservationNumber + 1 <= 400, "Max pre-whitelist reached");
+    currentReservationNumber = currentReservationNumber + 1;
+    reservationList[msg.sender] = true;
+}
+```
+
+### Mint
+
+Il n'existe qu'une seule fonction unique pour mint. Elle gère elle-même toutes les phases. 
+Pour pouvoir mint il faut être dans une des phases suivantes : "FreeMint", "ReservationMint", "FirstWhitelistMint", "SecondWhitelistMint", "PublicMint".
+La fonction va vérifier que l'ID que vous cherchez à mint existe et que l'ID en question n'a pas atteint sa supply maximale.
+
+Ensuite, en fonction de la phase, elle vérifiera, grâce à la preuve de Merkle (ou tableau de réservation), que vous avez le droit de mint durant la phase. 
+Si vous avez le droit de mint, elle vérifiera que la supply max n'est pas atteinte pour votre phase (il y aura + de whitelist que de NFT disponibles en phase de "First" et "Second" whitelist).
+Pour finir, elle vérifiera si vous avez envoyé assez d'Ethers et si vous n'avez pas déjà mint pendant la phase.
+
+Si toutes les conditions sont passées, votre NFT sera mint et un événement sera déclenché.
+
+```solidity
+function mint(uint idToMint, bytes32[] calldata _proof) public payable nonReentrant {
+    require(
+        currentStep == Step.FreeMint ||
+        currentStep == Step.ReservationMint ||
+        currentStep == Step.FirstWhitelistMint ||
+        currentStep == Step.SecondWhitelistMint ||
+        currentStep == Step.PublicMint
+    , "Sale is not open");
+    require(idToMint >= 1, "Nonexistent id");
+    require(idToMint <= maxToken, "Nonexistent id");
+    require(supplyByID[idToMint] + 1 <= maxSupply, "Max supply exceeded for this id");
+
+    if (currentStep == Step.FreeMint) {
+        require(isOnList(msg.sender, _proof, 0), "Not on free mint list");
+        require(totalSupply() + 1 <= 77, "Max free mint supply exceeded");
+        require(freeMintByWallet[msg.sender] + 1 <= 1, "You already minted your free NFT");
+        freeMintByWallet[msg.sender] += 1;
+        _mint(msg.sender, idToMint, 1, "");
+    } else if (currentStep == Step.ReservationMint) {
+        require(reservationList[msg.sender], "Not on reservation list");
+        require(msg.value >= reservationNFTPrice, "Not enought ether");
+        require(totalSupply() + 1 <= 477, "Max reservation mint supply exceeded");
+        require(reservationMintByWallet[msg.sender] + 1 <= 1, "You already minted your reserved NFT");
+        reservationMintByWallet[msg.sender] += 1;
+        _mint(msg.sender, idToMint, 1, "");
+    } else if (currentStep == Step.FirstWhitelistMint) {
+        require(isOnList(msg.sender, _proof, 1), "Not on first whitelist");
+        require(msg.value >= whitelistPrice, "Not enought ether");
+        require(totalSupply() + 1 <= 577, "Max first whitelist mint supply exceeded");
+        require(firstWhitelistMintByWallet[msg.sender] + 1 <= 1, "You already minted your first whitelist NFT");
+        firstWhitelistMintByWallet[msg.sender] += 1;
+        _mint(msg.sender, idToMint, 1, "");
+    } else if (currentStep == Step.SecondWhitelistMint) {
+        require(isOnList(msg.sender, _proof, 2), "Not on second whitelist");
+        require(msg.value >= whitelistPrice, "Not enought ether");
+        require(totalSupply() + 1 <= 777, "Max second whitelist mint supply exceeded");
+        require(secondWhitelistMintByWallet[msg.sender] + 1 <= 1, "You already minted your second whitelist NFT");
+        secondWhitelistMintByWallet[msg.sender] += 1;
+        _mint(msg.sender, idToMint, 1, "");
+    } else {
+        require(msg.value >= publicPrice, "Not enought ether");
+        require(totalSupply() + 1 <= 777, "Max second whitelist mint supply exceeded");
+        _mint(msg.sender, idToMint, 1, "");
+    }
+    supplyByID[idToMint]++;
+    emit newMint(msg.sender, idToMint);
+}
+```
+
+### Autres Fonctions
+
+Les autres fonctions non listées ici permettent de consulter certaines informations, au propriétaire du contrat, de modifier certaines variables, etc...
+Toutes les fonctions sont dans le contrat, il est conseillé de lire le contrat en entier, des commentaires sont présents pour vous aider à comprendre. 
+## Auteur
+
+- [@0xNekr](https://www.github.com/octokatherine)
---
+++ contracts/MarmottoshisIsERC1155.sol
@@ -0,0 +1,417 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
+import "@openzeppelin/contracts/token/common/ERC2981.sol";
+
+contract MarmottoshisIsERC1155 is ERC1155, ERC2981, Ownable, ReentrancyGuard {
+
+    using Strings for uint;
+
+    struct Artist {
+        string name;
+        string link;
+    }
+
+    enum Step {
+        SaleNotStarted,
+        WLReservation,
+        FreeMint,
+        ReservationMint,
+        FirstWhitelistMint,
+        SecondWhitelistMint,
+        PublicMint,
+        SoldOut
+    }
+
+    Step public currentStep;
+
+    uint public constant maxToken = 21; // 21 different NFTs
+    uint public constant maxSupply = 37; // 37 copies of each NFT
+
+    uint public reservationPrice = 0.01 ether; // Price of the reservation
+
+    uint public reservationNFTPrice = 0.008 ether; // Price of the NFT for reservation list
+    uint public whitelistPrice = 0.02 ether; // Price of whitelist mint
+    uint public publicPrice = 0.03 ether; // Price of public mint
+
+    uint public balanceOfSatoshis = 0; // Balance of Satoshis (100000000 Satoshis = 1 Bitcoin)
+
+    uint public currentReservationNumber = 0; // Current number of reservations purchased
+
+    bytes32 public freeMintMerkleRoot; // Merkle root of the free mint
+    bytes32 public firstMerkleRoot; // Merkle root of the first whitelist
+    bytes32 public secondMerkleRoot; // Merkle root of the second whitelist
+
+    mapping(uint => Artist) public artistByID; // Artist by ID
+    mapping(uint => uint) public supplyByID; // Number of NFTs minted by ID
+    mapping(address => bool) public reservationList; // List of addresses that reserved (true = reserved)
+
+    mapping(address => uint) public freeMintByWallet; // Number of NFTs minted by wallet for free mint
+    mapping(address => uint) public reservationMintByWallet; // Number of reserved NFT mint by wallet
+    mapping(address => uint) public firstWhitelistMintByWallet; // Number of first whitelist NFT mint by wallet
+    mapping(address => uint) public secondWhitelistMintByWallet; // Number of second whitelist NFT mint by wallet
+
+    mapping(uint => uint) public balanceOfSatoshiByID; // Balance of Satoshi by token ID
+
+    address public marmott; // Marmott's address
+
+    bool public isMetadataLocked = false; // Locks the metadata URI
+    bool public isRevealed = false; // Reveal the NFTs
+
+    event newRedeemRequest(address indexed sender, uint256 nftIdRedeemed, uint256 burnAmount, string btcAddress, uint256 satoshisAmount); // Redeem event
+    event newMint(address indexed sender, uint256 nftIdMinted); // Mint event
+    event stepUpdated(Step currentStep); // Step update event
+
+    constructor(address _marmott, string memory _uri)
+    ERC1155(_uri)
+    {
+        marmott = _marmott;
+    }
+
+    /*
+    * @notice Mints a new token to msg.sender
+    * @param uint : Id of NFT to mint.
+    * @param bytes32[] : Proof of whitelist (could be empty []).
+    */
+    function mint(uint idToMint, bytes32[] calldata _proof) public payable nonReentrant {
+        require(
+            currentStep == Step.FreeMint ||
+            currentStep == Step.ReservationMint ||
+            currentStep == Step.FirstWhitelistMint ||
+            currentStep == Step.SecondWhitelistMint ||
+            currentStep == Step.PublicMint
+        , "Sale is not open");
+        require(idToMint >= 1, "Nonexistent id");
+        require(idToMint <= maxToken, "Nonexistent id");
+        require(supplyByID[idToMint] + 1 <= maxSupply, "Max supply exceeded for this id");
+
+        if (currentStep == Step.FreeMint) {
+            require(isOnList(msg.sender, _proof, 0), "Not on free mint list");
+            require(totalSupply() + 1 <= 77, "Max free mint supply exceeded");
+            require(freeMintByWallet[msg.sender] + 1 <= 1, "You already minted your free NFT");
+            freeMintByWallet[msg.sender] += 1;
+            _mint(msg.sender, idToMint, 1, "");
+        } else if (currentStep == Step.ReservationMint) {
+            require(reservationList[msg.sender], "Not on reservation list");
+            require(msg.value >= reservationNFTPrice, "Not enought ether");
+            require(totalSupply() + 1 <= 477, "Max reservation mint supply exceeded");
+            require(reservationMintByWallet[msg.sender] + 1 <= 1, "You already minted your reserved NFT");
+            reservationMintByWallet[msg.sender] += 1;
+            _mint(msg.sender, idToMint, 1, "");
+        } else if (currentStep == Step.FirstWhitelistMint) {
+            require(isOnList(msg.sender, _proof, 1), "Not on first whitelist");
+            require(msg.value >= whitelistPrice, "Not enought ether");
+            require(totalSupply() + 1 <= 577, "Max first whitelist mint supply exceeded");
+            require(firstWhitelistMintByWallet[msg.sender] + 1 <= 1, "You already minted your first whitelist NFT");
+            firstWhitelistMintByWallet[msg.sender] += 1;
+            _mint(msg.sender, idToMint, 1, "");
+        } else if (currentStep == Step.SecondWhitelistMint) {
+            require(isOnList(msg.sender, _proof, 2), "Not on second whitelist");
+            require(msg.value >= whitelistPrice, "Not enought ether");
+            require(totalSupply() + 1 <= 777, "Max second whitelist mint supply exceeded");
+            require(secondWhitelistMintByWallet[msg.sender] + 1 <= 1, "You already minted your second whitelist NFT");
+            secondWhitelistMintByWallet[msg.sender] += 1;
+            _mint(msg.sender, idToMint, 1, "");
+        } else {
+            require(msg.value >= publicPrice, "Not enought ether");
+            require(totalSupply() + 1 <= 777, "Max second whitelist mint supply exceeded");
+            _mint(msg.sender, idToMint, 1, "");
+        }
+        supplyByID[idToMint]++;
+        emit newMint(msg.sender, idToMint);
+    }
+
+    /*
+    * @notice update step
+    * @param _step step to update
+    */
+    function updateStep(Step _step) external onlyOwner {
+        currentStep = _step;
+        emit stepUpdated(currentStep);
+    }
+
+    /*
+    * @notice update Marmott's address
+    * @param address : new Marmott's address
+    */
+    function updateMarmott(address _marmott) external {
+        require(msg.sender == marmott || msg.sender == owner(), "Only Marmott or owner can update Marmott");
+        marmott = _marmott;
+    }
+
+    /*
+    * @notice lock metadata
+    */
+    function lockMetadata() external onlyOwner {
+        isMetadataLocked = true;
+    }
+
+    /*
+    * @notice reveal NFTs
+    */
+    function reveal() external onlyOwner {
+        isRevealed = true;
+    }
+
+    /*
+    * @notice update URI
+    * @param string : new URI
+    */
+    function updateURI(string memory _newUri) external onlyOwner {
+        require(!isMetadataLocked, "Metadata locked");
+        _uri = _newUri;
+    }
+
+    /*
+    * @notice add satoshis to balanceOfSatoshis
+    * @param uint : amount of satoshis to add
+    */
+    function addBTC(uint satoshis) external {
+        require(msg.sender == marmott, "Only Marmott can add BTC");
+        balanceOfSatoshis = balanceOfSatoshis + satoshis;
+        uint divedBy = getNumberOfIdLeft();
+        require(divedBy > 0, "No NFT left");
+        uint satoshisPerId = satoshis / divedBy;
+        for (uint i = 1; i <= maxToken; i++) {
+            if (supplyByID[i] > 0) {
+                balanceOfSatoshiByID[i] = balanceOfSatoshiByID[i] + satoshisPerId;
+            }
+        }
+    }
+
+    /*
+    * @notice remove satoshis from balanceOfSatoshis
+    * @param uint : amount of satoshis to remove
+    */
+    function subBTC(uint satoshis) external {
+        require(msg.sender == marmott, "Only Marmott can sub BTC");
+        balanceOfSatoshis = balanceOfSatoshis - satoshis;
+        uint divedBy = getNumberOfIdLeft();
+        require(divedBy > 0, "No NFT left");
+        uint satoshisPerId = satoshis / divedBy;
+        for (uint i = 1; i <= maxToken; i++) {
+            if (supplyByID[i] > 0) {
+                balanceOfSatoshiByID[i] = balanceOfSatoshiByID[i] - satoshisPerId;
+            }
+        }
+    }
+
+    /*
+    * @notice redeem satoshis and burn NFT
+    * @param uint : id of NFT to burn/redeem
+    * @param string : bitcoin address to send satoshis to
+    */
+    function burnAndRedeem(uint _idToRedeem, string memory _btcAddress) public nonReentrant {
+        require(_idToRedeem >= 1, "Nonexistent id");
+        require(_idToRedeem <= maxToken, "Nonexistent id");
+        require(balanceOf(msg.sender, _idToRedeem) >= 1, "Not enough Marmott to burn");
+        _burn(msg.sender, _idToRedeem, 1);
+        uint satoshisToRedeem = redeemableById(_idToRedeem);
+        balanceOfSatoshis = balanceOfSatoshis - satoshisToRedeem;
+        balanceOfSatoshiByID[_idToRedeem] = balanceOfSatoshiByID[_idToRedeem] - satoshisToRedeem;
+        supplyByID[_idToRedeem] = supplyByID[_idToRedeem] - 1;
+        emit newRedeemRequest(msg.sender, _idToRedeem, 1, _btcAddress, satoshisToRedeem);
+    }
+
+    /*
+    * @notice function for user to be preWhitelist
+    */
+    function reservationForWhitelist() external payable nonReentrant {
+        require(currentStep == Step.WLReservation, "Reservation for whitelist is not open");
+        require(msg.value >= reservationPrice, "Not enought ether");
+        require(reservationList[msg.sender] == false, "You are already in the pre-whitelist");
+        require(currentReservationNumber + 1 <= 400, "Max pre-whitelist reached");
+        currentReservationNumber = currentReservationNumber + 1;
+        reservationList[msg.sender] = true;
+    }
+
+    /*
+    * @notice get number of Satoshis redeemable by NFT ID
+    * @param uint : id of NFT
+    */
+    function redeemableById(uint _id) public view returns (uint) {
+        if (supplyByID[_id] == 0) {
+            return 0;
+        } else {
+            return balanceOfSatoshiByID[_id] / supplyByID[_id];
+        }
+    }
+
+    /*
+    * @notice get number of NFT's ID with supply left
+    */
+    function getNumberOfIdLeft() public view returns (uint) {
+        uint numberOfIdLeft = 0;
+        for (uint i = 1; i <= maxToken; i++) {
+            if (supplyByID[i] > 0) {
+                numberOfIdLeft = numberOfIdLeft + 1;
+            }
+        }
+        return numberOfIdLeft;
+    }
+
+    /*
+    * @notice create Artist struct of an artist and add it in artistByID mapping
+    * @param _artists[] : array of artist's name
+    * @param _links[] : array of artist's link
+    * @param _id : id of the artist's NFT
+    */
+    function addArtist(string[] memory _artists, string[] memory _links, uint[] memory _id) public onlyOwner {
+        require(_artists.length == _links.length && _artists.length == _id.length, "Artists and links must be the same length");
+        for (uint i = 0; i < _artists.length; i++) {
+            artistByID[_id[i]] = Artist({
+            name : _artists[i],
+            link : _links[i]
+            });
+        }
+    }
+
+    /*
+    * @notice update free mint merkle root
+    * @param _merkleRoot : new merkle root
+    */
+    function updateFreeMintMerkleRoot(bytes32 _merkleRoot) external onlyOwner {
+        freeMintMerkleRoot = _merkleRoot;
+    }
+
+    /*
+    * @notice update first whitelist merkle root
+    * @param _merkleRoot : new merkle root
+    */
+    function updateFirstWhitelistMerkleRoot(bytes32 _merkleRoot) external onlyOwner {
+        firstMerkleRoot = _merkleRoot;
+    }
+
+    /*
+    * @notice update second whitelist merkle root
+    * @param _merkleRoot : new merkle root
+    */
+    function updateSecondWhitelistMerkleRoot(bytes32 _merkleRoot) external onlyOwner {
+        secondMerkleRoot = _merkleRoot;
+    }
+
+    /*
+    * @notice update reservation price
+    * @param _reservationPrice : new reservation price
+    */
+    function updateReservationPrice(uint _reservationPrice) external onlyOwner {
+        reservationPrice = _reservationPrice;
+    }
+
+    /*
+    * @notice update reservation NFT price
+    * @param _reservationNFTPrice : new reservation NFT price
+    */
+    function updateReservationNFTPrice(uint _reservationNFTPrice) external onlyOwner {
+        reservationNFTPrice = _reservationNFTPrice;
+    }
+
+    /*
+    * @notice update whitelist price
+    * @param _whitelistPrice : new whitelist price
+    */
+    function updateWLPrice(uint _whitelistPrice) external onlyOwner {
+        whitelistPrice = _whitelistPrice;
+    }
+
+    /*
+    * @notice update public price
+    * @param _publicPrice : new public price
+    */
+    function updatePublicPrice(uint _publicPrice) external onlyOwner {
+        publicPrice = _publicPrice;
+    }
+
+    /*
+    * @notice return NFTs URI
+    * @param _tokenId : id of NFT
+    */
+    function uri(uint256 _tokenId) override public view returns (string memory) {
+        require(_tokenId >= 1, "Nonexistent id");
+        require(_tokenId <= maxToken, "Nonexistent id");
+        require(supplyByID[_tokenId] > 0, "Nonexistent id");
+        string memory image = string(abi.encodePacked(_uri, _tokenId.toString(), ".png"));
+        string memory name = string(abi.encodePacked("Marmotoshis #", _tokenId.toString()));
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{"name": "', name, '", "image": "', image, '", "description": "Realised by ', artistByID[_tokenId].name, ' you can see more here : ', artistByID[_tokenId].link, '", "attributes": [{"trait_type": "Satoshis", "value": "', redeemableById(_tokenId).toString(), '"}]}'
+                    )
+                )
+            )
+        );
+
+        return string(abi.encodePacked('data:application/json;base64,', json));
+    }
+
+    /*
+    * @notice Returns the sum of all supplies for each NFT ID
+    */
+    function totalSupply() public view returns (uint) {
+        uint supply = 0;
+        for (uint i = 1; i <= maxToken; i++) {
+            supply = supply + supplyByID[i];
+        }
+        return supply;
+    }
+
+    /*
+    * @notice withdraw ether from contract
+    */
+    function withdraw() external onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+
+    /*
+    * @notice know if user is on a list
+    * @param _account address of user
+    * @param _proof Merkle proof
+    * @param _step : step of the list (0 = free mint, 1 = first whitelist, 2 = second whitelist)
+    */
+    function isOnList(address _account, bytes32[] calldata _proof, uint _step) public view returns (bool) {
+        if (_step == 0) {
+            return _verify(_leaf(_account), _proof, freeMintMerkleRoot);
+        } else if (_step == 1) {
+            return _verify(_leaf(_account), _proof, firstMerkleRoot);
+        } else if (_step == 2) {
+            return _verify(_leaf(_account), _proof, secondMerkleRoot);
+        } else {
+            return false;
+        }
+    }
+
+    /*
+    * @notice get merkle _leaf
+    * @param _account address of user
+    */
+    function _leaf(address _account) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked(_account));
+    }
+
+    /*
+    * @notice verify if user is on list
+    * @param leaf bytes32 leaf of merkle tree
+    * @param proof bytes32 Merkle proof
+    * @param root bytes32 Merkle root
+    */
+    function _verify(bytes32 leaf, bytes32[] memory proof, bytes32 root) internal pure returns (bool) {
+        return MerkleProof.verify(proof, root, leaf);
+    }
+
+    // EIP2981 royalties
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC1155, ERC2981) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function setDefaultRoyalty(address receiver, uint96 feeNumerator) external onlyOwner {
+        _setDefaultRoyalty(receiver, feeNumerator);
+    }
+    // END OF EIP2981 royalties
+}
