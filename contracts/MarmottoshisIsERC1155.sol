c4a1f79b4a6bb923239d2c15bbf54c66884924e3
0xNekr <0xNekr.dev@gmail.com>
2022-12-02 12:53:46 +0100

    update: add interface IMarmottoshisIsERC1155.sol. Now comments are in this interface. + some fix

--- contracts/MarmottoshisIsERC1155.sol
+++ contracts/MarmottoshisIsERC1155.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+import "./IMarmottoshisIsERC1155.sol";
 import "./ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
@@ -9,25 +10,9 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
 import "@openzeppelin/contracts/token/common/ERC2981.sol";
 
-contract MarmottoshisIsERC1155 is ERC1155, ERC2981, Ownable, ReentrancyGuard {
+contract MarmottoshisIsERC1155 is IMarmottoshisIsERC1155, ERC1155, ERC2981, Ownable, ReentrancyGuard {
 
     using Strings for uint;
-
-    struct Artist {
-        string name;
-        string link;
-    }
-
-    enum Step {
-        SaleNotStarted,
-        WLReservation,
-        FreeMint,
-        ReservationMint,
-        FirstWhitelistMint,
-        SecondWhitelistMint,
-        PublicMint,
-        SoldOut
-    }
 
     Step public currentStep;
 
@@ -64,21 +49,13 @@ contract MarmottoshisIsERC1155 is ERC1155, ERC2981, Ownable, ReentrancyGuard {
     bool public isMetadataLocked = false; // Locks the metadata URI
     bool public isRevealed = false; // Reveal the NFTs
 
-    event newRedeemRequest(address indexed sender, uint256 nftIdRedeemed, uint256 burnAmount, string btcAddress, uint256 satoshisAmount); // Redeem event
-    event newMint(address indexed sender, uint256 nftIdMinted); // Mint event
-    event stepUpdated(Step currentStep); // Step update event
-
     constructor(address _marmott, string memory _uri)
     ERC1155(_uri)
     {
         marmott = _marmott;
     }
 
-    /*
-    * @notice Mints a new token to msg.sender
-    * @param uint : Id of NFT to mint.
-    * @param bytes32[] : Proof of whitelist (could be empty []).
-    */
+    // @dev see {IMarmottoshisIsERC1155-mint}
     function mint(uint idToMint, bytes32[] calldata _proof) public payable nonReentrant {
         require(
             currentStep == Step.FreeMint ||
@@ -127,52 +104,36 @@ contract MarmottoshisIsERC1155 is ERC1155, ERC2981, Ownable, ReentrancyGuard {
         emit newMint(msg.sender, idToMint);
     }
 
-    /*
-    * @notice update step
-    * @param _step step to update
-    */
+    // @dev see {IMarmottoshisIsERC1155-updateStep}
     function updateStep(Step _step) external onlyOwner {
         currentStep = _step;
         emit stepUpdated(currentStep);
     }
 
-    /*
-    * @notice update Marmott's address
-    * @param address : new Marmott's address
-    */
+    // @dev see {IMarmottoshisIsERC1155-updateMarmott}
     function updateMarmott(address _marmott) external {
         require(msg.sender == marmott || msg.sender == owner(), "Only Marmott or owner can update Marmott");
         marmott = _marmott;
     }
 
-    /*
-    * @notice lock metadata
-    */
+    // @dev see {IMarmottoshisIsERC1155-lockMetadata}
     function lockMetadata() external onlyOwner {
         isMetadataLocked = true;
     }
 
-    /*
-    * @notice reveal NFTs
-    */
+    // @dev see {IMarmottoshisIsERC1155-reveal}
     function reveal() external onlyOwner {
         isRevealed = true;
     }
 
-    /*
-    * @notice update URI
-    * @param string : new URI
-    */
+    // @dev see {IMarmottoshisIsERC1155-updateURI}
     function updateURI(string memory _newUri) external onlyOwner {
         require(!isMetadataLocked, "Metadata locked");
         _uri = _newUri;
     }
 
-    /*
-    * @notice add satoshis to balanceOfSatoshis
-    * @param uint : amount of satoshis to add
-    */
-    function addBTC(uint satoshis) external {
+    // @dev see {IMarmottoshisIsERC1155-addSats}
+    function addSats(uint satoshis) external {
         require(msg.sender == marmott, "Only Marmott can add BTC");
         balanceOfSatoshis = balanceOfSatoshis + satoshis;
         uint divedBy = getNumberOfIdLeft();
@@ -185,11 +146,8 @@ contract MarmottoshisIsERC1155 is ERC1155, ERC2981, Ownable, ReentrancyGuard {
         }
     }
 
-    /*
-    * @notice remove satoshis from balanceOfSatoshis
-    * @param uint : amount of satoshis to remove
-    */
-    function subBTC(uint satoshis) external {
+    // @dev see {IMarmottoshisIsERC1155-subSats}
+    function subSats(uint satoshis) external {
         require(msg.sender == marmott, "Only Marmott can sub BTC");
         require(balanceOfSatoshis >= satoshis, "Not enough satoshis in balance to sub");
         balanceOfSatoshis = balanceOfSatoshis - satoshis;
@@ -204,11 +162,7 @@ contract MarmottoshisIsERC1155 is ERC1155, ERC2981, Ownable, ReentrancyGuard {
         }
     }
 
-    /*
-    * @notice redeem satoshis and burn NFT
-    * @param uint : id of NFT to burn/redeem
-    * @param string : bitcoin address to send satoshis to
-    */
+    // @dev see {IMarmottoshisIsERC1155-burnAndRedeem}
     function burnAndRedeem(uint _idToRedeem, string memory _btcAddress) public nonReentrant {
         require(_idToRedeem >= 1, "Nonexistent id");
         require(_idToRedeem <= maxToken, "Nonexistent id");
@@ -223,9 +177,7 @@ contract MarmottoshisIsERC1155 is ERC1155, ERC2981, Ownable, ReentrancyGuard {
         emit newRedeemRequest(msg.sender, _idToRedeem, 1, _btcAddress, satoshisToRedeem);
     }
 
-    /*
-    * @notice function for user to be preWhitelist
-    */
+    // @dev see {IMarmottoshisIsERC1155-reservationForWhitelist}
     function reservationForWhitelist() external payable nonReentrant {
         require(currentStep == Step.WLReservation, "Reservation for whitelist is not open");
         require(msg.value >= reservationPrice, "Not enough ether");
@@ -235,10 +187,7 @@ contract MarmottoshisIsERC1155 is ERC1155, ERC2981, Ownable, ReentrancyGuard {
         reservationList[msg.sender] = true;
     }
 
-    /*
-    * @notice get number of Satoshis redeemable by NFT ID
-    * @param uint : id of NFT
-    */
+    // @dev see {IMarmottoshisIsERC1155-redeemableById}
     function redeemableById(uint _id) public view returns (uint) {
         if (supplyByID[_id] == 0) {
             return 0;
@@ -247,9 +196,7 @@ contract MarmottoshisIsERC1155 is ERC1155, ERC2981, Ownable, ReentrancyGuard {
         }
     }
 
-    /*
-    * @notice get number of NFT's ID with supply left
-    */
+    // @dev see {IMarmottoshisIsERC1155-getNumberOfIdLeft}
     function getNumberOfIdLeft() public view returns (uint) {
         uint numberOfIdLeft = 0;
         for (uint i = 1; i <= maxToken; i++) {
@@ -260,12 +207,7 @@ contract MarmottoshisIsERC1155 is ERC1155, ERC2981, Ownable, ReentrancyGuard {
         return numberOfIdLeft;
     }
 
-    /*
-    * @notice create Artist struct of an artist and add it in artistByID mapping
-    * @param _artists[] : array of artist's name
-    * @param _links[] : array of artist's link
-    * @param _id : id of the artist's NFT
-    */
+    // @dev see {IMarmottoshisIsERC1155-addArtist}
     function addArtist(string[] memory _artists, string[] memory _links, uint[] memory _id) external onlyOwner {
         require(_artists.length == _links.length && _artists.length == _id.length, "Artists and links must be the same length");
         for (uint i = 0; i < _artists.length; i++) {
@@ -276,66 +218,42 @@ contract MarmottoshisIsERC1155 is ERC1155, ERC2981, Ownable, ReentrancyGuard {
         }
     }
 
-    /*
-    * @notice update free mint merkle root
-    * @param _merkleRoot : new merkle root
-    */
+    // @dev see {IMarmottoshisIsERC1155-updateFreeMintMerkleRoot}
     function updateFreeMintMerkleRoot(bytes32 _merkleRoot) external onlyOwner {
         freeMintMerkleRoot = _merkleRoot;
     }
 
-    /*
-    * @notice update first whitelist merkle root
-    * @param _merkleRoot : new merkle root
-    */
+    // @dev see {IMarmottoshisIsERC1155-updateFirstWhitelistMerkleRoot}
     function updateFirstWhitelistMerkleRoot(bytes32 _merkleRoot) external onlyOwner {
         firstMerkleRoot = _merkleRoot;
     }
 
-    /*
-    * @notice update second whitelist merkle root
-    * @param _merkleRoot : new merkle root
-    */
+    // @dev see {IMarmottoshisIsERC1155-updateSecondWhitelistMerkleRoot}
     function updateSecondWhitelistMerkleRoot(bytes32 _merkleRoot) external onlyOwner {
         secondMerkleRoot = _merkleRoot;
     }
 
-    /*
-    * @notice update reservation price
-    * @param _reservationPrice : new reservation price
-    */
+    // @dev see {IMarmottoshisIsERC1155-updateReservationPrice}
     function updateReservationPrice(uint _reservationPrice) external onlyOwner {
         reservationPrice = _reservationPrice;
     }
 
-    /*
-    * @notice update reservation NFT price
-    * @param _reservationNFTPrice : new reservation NFT price
-    */
+    // @dev see {IMarmottoshisIsERC1155-updateReservationNFTPrice}
     function updateReservationNFTPrice(uint _reservationNFTPrice) external onlyOwner {
         reservationNFTPrice = _reservationNFTPrice;
     }
 
-    /*
-    * @notice update whitelist price
-    * @param _whitelistPrice : new whitelist price
-    */
+    // @dev see {IMarmottoshisIsERC1155-updateWLPrice}
     function updateWLPrice(uint _whitelistPrice) external onlyOwner {
         whitelistPrice = _whitelistPrice;
     }
 
-    /*
-    * @notice update public price
-    * @param _publicPrice : new public price
-    */
+    // @dev see {IMarmottoshisIsERC1155-updatePublicPrice}
     function updatePublicPrice(uint _publicPrice) external onlyOwner {
         publicPrice = _publicPrice;
     }
 
-    /*
-    * @notice return NFTs URI
-    * @param _tokenId : id of NFT
-    */
+    // @dev see {IERC1155MetadataURI-uri}
     function uri(uint256 _tokenId) override public view returns (string memory) {
         require(_tokenId >= 1, "Nonexistent id");
         require(_tokenId <= maxToken, "Nonexistent id");
@@ -344,12 +262,12 @@ contract MarmottoshisIsERC1155 is ERC1155, ERC2981, Ownable, ReentrancyGuard {
             return _uri;
         }
         string memory image = string(abi.encodePacked(_uri, _tokenId.toString(), ".png"));
-        string memory name = string(abi.encodePacked("Marmottoshis #", _tokenId.toString()));
+        string memory name = string(abi.encodePacked("Marmottoshi #", _tokenId.toString()));
         string memory json = Base64.encode(
             bytes(
                 string(
                     abi.encodePacked(
-                        '{"name": "', name, '", "image": "', image, '", "description": "Realised by ', artistByID[_tokenId].name, ' you can see more here : ', artistByID[_tokenId].link, '", "attributes": [{"trait_type": "Satoshis", "value": "', redeemableById(_tokenId).toString(), '"}]}'
+                        '{"name": "', name, '", "image": "', image, '", "description": "Realised by ', artistByID[_tokenId].name, '. You can see more here : ', artistByID[_tokenId].link, ' .", "attributes": [{"trait_type": "Satoshis", "value": "', redeemableById(_tokenId).toString(), '"}, {"trait_type": "Remaining Copy", "value": "', supplyByID[_tokenId].toString(), '"}]}'
                     )
                 )
             )
@@ -358,9 +276,7 @@ contract MarmottoshisIsERC1155 is ERC1155, ERC2981, Ownable, ReentrancyGuard {
         return string(abi.encodePacked('data:application/json;base64,', json));
     }
 
-    /*
-    * @notice Returns the sum of all supplies for each NFT ID
-    */
+    // @dev see {IMarmottoshisIsERC1155-totalSupply}
     function totalSupply() public view returns (uint) {
         uint supply = 0;
         for (uint i = 1; i <= maxToken; i++) {
@@ -369,19 +285,12 @@ contract MarmottoshisIsERC1155 is ERC1155, ERC2981, Ownable, ReentrancyGuard {
         return supply;
     }
 
-    /*
-    * @notice withdraw ether from contract
-    */
+    // @dev see {IMarmottoshisIsERC1155-withdraw}
     function withdraw() external onlyOwner {
         payable(msg.sender).transfer(address(this).balance);
     }
 
-    /*
-    * @notice know if user is on a list
-    * @param _account address of user
-    * @param _proof Merkle proof
-    * @param _step : step of the list (0 = free mint, 1 = first whitelist, 2 = second whitelist)
-    */
+    // @dev see {IMarmottoshisIsERC1155-isOnList}
     function isOnList(address _account, bytes32[] calldata _proof, uint _step) public view returns (bool) {
         if (_step == 0) {
             return _verify(_leaf(_account), _proof, freeMintMerkleRoot);
@@ -394,31 +303,22 @@ contract MarmottoshisIsERC1155 is ERC1155, ERC2981, Ownable, ReentrancyGuard {
         }
     }
 
-    /*
-    * @notice get merkle _leaf
-    * @param _account address of user
-    */
     function _leaf(address _account) internal pure returns (bytes32) {
         return keccak256(abi.encodePacked(_account));
     }
 
-    /*
-    * @notice verify if user is on list
-    * @param leaf bytes32 leaf of merkle tree
-    * @param proof bytes32 Merkle proof
-    * @param root bytes32 Merkle root
-    */
     function _verify(bytes32 leaf, bytes32[] memory proof, bytes32 root) internal pure returns (bool) {
         return MerkleProof.verify(proof, root, leaf);
     }
 
-    // EIP2981 royalties
+
+    // @dev see {IERC165-supportsInterface}.
     function supportsInterface(bytes4 interfaceId) public view virtual override(ERC1155, ERC2981) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 
+    // @dev see {IMarmottoshisIsERC1155-setDefaultRoyalty}
     function setDefaultRoyalty(address receiver, uint96 feeNumerator) external onlyOwner {
         _setDefaultRoyalty(receiver, feeNumerator);
     }
-    // END OF EIP2981 royalties
 }
